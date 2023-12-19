b55a0c94e0aa10483c6e8ed75a6a0a22d15ed461
M_K_J <kevin911012@gmail.com>
2023-12-19 23:26:20 +0800

    完成DoctorHelper屬性、操作內容

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/DoctorHelper.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/DoctorHelper.java
@@ -6,16 +6,9 @@ import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
 
-// TODO: Auto-generated Javadoc
 /**
- * <p>
- * The Class DoctorHelper<br>
+ * The Class DoctorHelper
  * DoctorHelper類別（class）主要管理所有與Doctor相關與資料庫之方法（method）
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @since 1.0.0
  */
 
 public class DoctorHelper {
@@ -57,7 +50,74 @@ public class DoctorHelper {
      * @return the JSONObject 回傳SQL執行結果與自資料庫取回之所有資料
      */
     public JSONObject getAll() {
+        /** 新建一個 Doctor 物件之 d 變數，用於紀錄每一位查詢回之醫生資料 */
+        Doctor d = null;
+        /** 用於儲存所有檢索回之醫生，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `hospital`.`doctor`";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while (rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int doctor_id = rs.getInt("doctor_id");
+                String doctor_name = rs.getString("doctor_name");
+                int dept_id = rs.getInt("dept_id");
+
+                /** 將每一筆醫生資料產生一名新Doctor物件 */
+                d = new Doctor(doctor_id, doctor_name, dept_id);
+                /** 取出該醫生之資料並封裝至 JSONsonArray 內 */
+                jsa.put(d.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有醫生資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
     }
 
     /**
@@ -66,7 +126,74 @@ public class DoctorHelper {
      * @return the JSON object 回傳SQL執行結果與該編號之醫生資料
      */
     public JSONObject getByID(int id) {
+        JSONObject data = new JSONObject();
+        Doctor d = null;
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `hosptial`.`doctor` WHERE `doctor`.`doctor_id` = ? LIMIT 1";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while (rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int doctor_id = rs.getInt("doctor_id");
+                String doctor_name = rs.getString("doctor_name");
+                int dept_id = rs.getInt("dept_id");
+
+                /** 將每一筆醫生資料產生一名新Doctor物件 */
+                d = new Doctor(doctor_id, doctor_name, dept_id);
+                /** 取出該項醫生之資料並封裝至 JSONsonArray 內 */
+                data = d.getData();
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有診室資料之JSONArray，封裝成JSONObject回傳 */
+
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", data);
+
+        return response;
     }
 
 }
