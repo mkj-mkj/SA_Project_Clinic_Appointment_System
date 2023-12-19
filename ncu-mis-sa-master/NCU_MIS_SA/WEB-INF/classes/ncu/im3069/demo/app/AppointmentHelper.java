c1c9f921a1ff964f97a07c3c6bd64aba4bc88320
M_K_J <kevin911012@gmail.com>
2023-12-20 04:10:55 +0800

    完成AppointmentHelper屬性、方法內容，取消預約從輸入身分證字號改成輸入身分證字號和醫生編號

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/AppointmentHelper.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/AppointmentHelper.java
@@ -6,16 +6,9 @@ import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
 
-// TODO: Auto-generated Javadoc
 /**
- * <p>
- * The Class AppointmentHelper<br>
+ * The Class AppointmentHelper
  * AppointmentHelper類別（class）主要管理所有與Appointment相關與資料庫之方法（method）
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @since 1.0.0
  */
 
 public class AppointmentHelper {
@@ -58,8 +51,57 @@ public class AppointmentHelper {
      * 
      * @return the JSONObject 回傳SQL執行結果
      */
-    public JSONObject deleteByID(String id) {
+    public JSONObject cancel(String user_id, int doctor_id) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+
+            /** SQL指令 */
+            String sql = "DELETE FROM `hospital`.`appointment` WHERE `user_id` = ? and `doctor_id` = ? LIMIT 1";
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, user_id);
+            pres.setInt(2, doctor_id);
+            /** 執行刪除之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+
+        return response;
     }
 
     /**
@@ -68,7 +110,76 @@ public class AppointmentHelper {
      * @return the JSONObject 回傳SQL執行結果與自資料庫取回之所有資料
      */
     public JSONObject getAll() {
+        /** 新建一個 Appointment 物件之 ap 變數，用於紀錄每一位查詢回之預約資料 */
+        Appointment ap = null;
+        /** 用於儲存所有檢索回之門診，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `hospital`.`appointment`";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while (rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int seq = rs.getInt("appointment_seq");
+                int doctor_id = rs.getInt("doctor_id");
+                String user_id = rs.getString("user_id");
+                Timestamp reserve_time = rs.getTimestamp("reserve_time");
+                int appointment_number = rs.getInt("appointment");
+
+                /** 將每一筆預約資料產生一個新Appointment物件 */
+                ap = new Appointment(seq, doctor_id, user_id, reserve_time, appointment_number);
+                /** 取出該預約資料並封裝至 JSONsonArray 內 */
+                jsa.put(ap.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有門診資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
     }
 
     /**
@@ -77,7 +188,118 @@ public class AppointmentHelper {
      * @return the JSON object 回傳SQL執行結果與該身分證字號之預約資料
      */
     public JSONObject getByID(String id) {
+        JSONObject data = new JSONObject();
+        Appointment ap = null;
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `hosptial`.`appointment` WHERE `appointment`.`user_id` = ? LIMIT 1";
+
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while (rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+
+                /** 將 ResultSet 之資料取出 */
+                int seq = rs.getInt("appointment_seq");
+                int doctor_id = rs.getInt("doctor_id");
+                String user_id = rs.getString("user_id");
+                Timestamp reserve_time = rs.getTimestamp("reserve_time");
+                int appointment_number = rs.getInt("appointment");
+
+                /** 將每一筆預約資料產生一名新Appointment物件 */
+                ap = new Appointment(seq, doctor_id, user_id, reserve_time, appointment_number);
+                /** 取出該項預約資料並封裝至 JSONsonArray 內 */
+                data = ap.getData();
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間、影響行數與所有門診資料之JSONArray，封裝成JSONObject回傳 */
+
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", data);
+
+        return response;
+    }
+
+    public int getAppointment(Appointment ap) {
+        /** 紀錄SQL總行數，若為「-1」代表資料庫檢索尚未完成 */
+        int row = -1;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT count(*) FROM `hostipal`.`appoint` WHERE `doctor_id` = ? and `reserve_time` = ?";
+
+            /** 取得所需之參數 */
+            int dcotor_id = ap.getDoctorID();
+            Timestamp reserve_time = ap.getReserveTime();
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, dcotor_id);
+            pres.setTimestamp(2, reserve_time);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 讓指標移往最後一列，取得目前有幾行在資料庫內 */
+            rs.next();
+            row = rs.getInt("count(*)");
+            System.out.print(row);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        return row + 1;
     }
 
     /**
@@ -86,16 +308,130 @@ public class AppointmentHelper {
      * @return the JSON object 回傳SQL指令執行之結果
      */
     public JSONObject create(Appointment ap) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "INSERT INTO `hostipal`.`appointment`(`appointment_seq`, `doctor_id`, `user_id`, `reserve_time`, `appointment`)"
+                    + " VALUES(?, ?, ?, ?, ?)";
+
+            /** 取得所需之參數 */
+            int appointment_seq = ap.getSeq();
+            int doctor_id = ap.getDoctorID();
+            String user_id = ap.getUserID();
+            Timestamp reserve_time = ap.getReserveTime();
+            int appointment_number = getAppointment(ap);
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, appointment_seq);
+            pres.setInt(2, doctor_id);
+            pres.setString(3, user_id);
+            pres.setTimestamp(4, reserve_time);
+            pres.setInt(5, appointment_number);
+
+            /** 執行新增之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
     }
 
     /**
-     * 更新預約資料
+     * 修改預約資料
      * 
      * @return the JSONObject 回傳SQL指令執行結果與執行之資料
      */
     public JSONObject update(Appointment ap) {
+        /** 紀錄回傳之資料 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
 
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "Update `hospital`.`appointment` SET `reserve_time` = ? ,`appointment` = ? WHERE `user_id` = ? and `doctor_id` = ?";
+            /** 取得所需之參數 */
+            String user_id = ap.getUserID();
+            int doctor_id = ap.getDoctorID();
+            Timestamp reserve_time = ap.getReserveTime();
+            int appointment_number = getAppointment(ap);
+
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setTimestamp(1, reserve_time);
+            pres.setInt(2, appointment_number);
+            pres.setString(3, user_id);
+            pres.setInt(4, doctor_id);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
     }
 
 }
