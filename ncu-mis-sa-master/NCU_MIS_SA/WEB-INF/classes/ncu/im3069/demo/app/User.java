007413e1e489de78fd33f573c09c3083a250cccf
M_K_J <kevin911012@gmail.com>
2023-12-20 01:44:09 +0800

    完成User屬性跟操作的內容，CaseNumber的設定改成從資料庫實作

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/User.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/User.java
@@ -7,14 +7,8 @@ import java.util.Calendar;
 
 // TODO: Auto-generated Javadoc
 /**
- * <p>
- * The Class Member
- * Member類別（class）具有會員所需要之屬性與方法，並且儲存與會員相關之商業判斷邏輯<br>
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @since 1.0.0
+ * The Class User
+ * User類別（class）具有使用者所需要之屬性與方法，並且儲存與使用者相關之商業判斷邏輯
  */
 
 public class User {
@@ -57,8 +51,7 @@ public class User {
     private UserHelper uh = UserHelper.getHelper();
 
     /**
-     * 實例化（Instantiates）一個新的（new）User物件<br>
-     * 採用多載（overload）方法進行，此建構子用於建立會員資料時，產生一名新的會員
+     * 實例化（Instantiates）一個新的（new）User物件
      */
     public User(String Id, String Name) {
         this.Id = Id;
@@ -67,7 +60,6 @@ public class User {
 
     /**
      * 實例化（Instantiates）一個新的（new）User物件<br>
-     * 採用多載（overload）方法進行，此建構子用於建立會員資料時，產生一名新的會員
      */
     public User(String Id, String Name, int CaseNumber) {
         this.Id = Id;
@@ -77,7 +69,6 @@ public class User {
 
     /**
      * 實例化（Instantiates）一個新的（new）User物件<br>
-     * 採用多載（overload）方法進行，此建構子用於建立會員資料時，產生一名新的會員
      */
     public User(String Id, String Name, int CaseNumber, String Address, Timestamp Birth, Boolean Gender, String Email,
             String Phone, String ResidenceTel, String BloodType, float Height, float Weight, String AllergyHistory,
@@ -104,74 +95,154 @@ public class User {
     /**
      * 取得使用者之身分證字號
      *
-     * @return the id 回傳會員編號
+     * @return the id 回傳身分證字號
      */
     public String getID() {
         return this.Id;
     }
 
+    /**
+     * 取得使用者之病歷號碼
+     *
+     * @return the id 回傳病歷號碼
+     */
     public int getCaseNumber() {
         return this.CaseNumber;
     }
 
+    /**
+     * 取得使用者之姓名
+     *
+     * @return the id 回傳使用者之姓名
+     */
     public String getName() {
-
+        return this.Name;
     }
 
+    /**
+     * 取得使用者之住址
+     *
+     * @return the id 回傳使用者之住址
+     */
     public String getAddress() {
-
+        return this.Address;
     }
 
+    /**
+     * 取得使用者之生日
+     *
+     * @return the id 回傳使用者之生日
+     */
     public Timestamp getBirth() {
-
+        return this.Birth;
     }
 
+    /**
+     * 取得使用者之性別
+     *
+     * @return the id 回傳使用者之性別 (1為女生，0為男生)
+     */
     public Boolean getGender() {
-
+        return this.Gender;
     }
 
+    /**
+     * 取得使用者之電子信箱
+     *
+     * @return the id 回傳使用者之電子信箱
+     */
     public String getEmail() {
-
+        return this.Email;
     }
 
+    /**
+     * 取得使用者之住電話號碼
+     *
+     * @return the id 回傳使用者之電話號碼
+     */
     public String getPhone() {
-
+        return this.Phone;
     }
 
+    /**
+     * 取得使用者之住家電話
+     *
+     * @return the id 回傳使用者之住家電話
+     */
     public String getResidenceTel() {
-
+        return this.ResidenceTel;
     }
 
+    /**
+     * 取得使用者之血型
+     *
+     * @return the id 回傳使用者之血型
+     */
     public String getBloodType() {
-
+        return this.BloodType;
     }
 
+    /**
+     * 取得使用者之身高
+     *
+     * @return the id 回傳使用者之身高
+     */
     public Float getHeight() {
-
+        return this.Height;
     }
 
+    /**
+     * 取得使用者之體重
+     *
+     * @return the id 回傳使用者之體重
+     */
     public Float getWeight() {
-
+        return this.Weight;
     }
 
+    /**
+     * 取得使用者之過敏史
+     *
+     * @return the id 回傳使用者之過敏史
+     */
     public String getAllergyHistory() {
-
+        return this.AllergyHistory;
     }
 
+    /**
+     * 取得使用者之重大疾病史
+     *
+     * @return the id 回傳使用者之重大疾病史
+     */
     public String getServerillHistory() {
-
+        return this.ServillHistory;
     }
 
+    /**
+     * 取得使用者之聯絡人姓名
+     *
+     * @return the id 回傳使用者之聯絡人姓名
+     */
     public String getContactName() {
-
+        return this.ContactName;
     }
 
+    /**
+     * 取得使用者之聯絡人關係
+     *
+     * @return the id 回傳使用者之聯絡人關係
+     */
     public String getContactRel() {
-
+        return this.ContactRel;
     }
 
+    /**
+     * 取得使用者之聯絡人電話
+     *
+     * @return the id 回傳使用者之聯絡人電話
+     */
     public String getContactTel() {
-
+        return this.ContactTel;
     }
 
     /**
@@ -180,7 +251,15 @@ public class User {
      * @return the JSON object 回傳SQL更新之結果與相關封裝之資料
      */
     public JSONObject update() {
+        /** 新建一個JSONObject用以儲存更新後之資料 */
+        JSONObject data = new JSONObject();
 
+        /** 檢查該名使用者是否已經在資料庫 */
+        if (this.Id != "") {
+            /** 透過UserHelper物件，更新目前之使用者資料置資料庫中 */
+            data = uh.update(this);
+        }
+        return data;
     }
 
     /**
@@ -189,13 +268,36 @@ public class User {
      * @return the data 取得該名使用噁之所有資料並封裝於JSONObject物件內
      */
     public JSONObject getData() {
+        /** 透過JSONObject將該名使用者所需之資料全部進行封裝 */
+        JSONObject jso = new JSONObject();
+        jso.put("user_id", getID());
+        jso.put("case_number", getCaseNumber());
+        jso.put("name", getName());
+        jso.put("user_address", getAddress());
+        jso.put("user_birth", getBirth());
+        jso.put("user_email", getEmail());
+        jso.put("user_gener", getGender());
+        jso.put("user_phone", getPhone());
+        jso.put("residence_tel", getResidenceTel());
+        jso.put("blood", getBloodType());
+        jso.put("height", getHeight());
+        jso.put("weight", getWeight());
+        jso.put("allergy_history", getAllergyHistory());
+        jso.put("serverill_history", getServerillHistory());
+        jso.put("contact_name", getContactName());
+        jso.put("contact_rel", getContactRel());
+        jso.put("contact_tel", getContactTel());
 
+        return jso;
     }
 
     /**
-     * 計算使用者之病例編號<br>
+     * 從資料庫實作好像比較好
+     * /**
+     * 計算使用者之病例編號
+     * 
+     * private void setCaseNumber() {
+     * 
+     * }
      */
-    private void setCaseNumber() {
-
-    }
 }
