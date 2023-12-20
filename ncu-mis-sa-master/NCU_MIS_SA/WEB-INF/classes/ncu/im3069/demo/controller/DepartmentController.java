8c5c88fe8109c2e5b4774f65209d0d3e2ba7d7c3
M_K_J <kevin911012@gmail.com>
2023-12-21 00:58:42 +0800

    完成DepartmentController 方法內容，不確定doPost要不要寫(待確

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/DepartmentController.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/DepartmentController.java
@@ -8,50 +8,75 @@ import ncu.im3069.demo.app.Department;
 import ncu.im3069.demo.app.DepartmentHelper;
 import ncu.im3069.tools.JsonReader;
 
-// TODO: Auto-generated Javadoc
 /**
- * <p>
  * The Class DepartmentController<br>
  * DepartmentController類別（class）主要用於處理Department相關之Http請求（Request），繼承HttpServlet
- * </p>
- * 
- * @author IPLab
- * @version 1.0.0
- * @since 1.0.0
  */
 
 public class DepartmentController extends HttpServlet {
 
-    /** The Constant serialVersionUID. */
-    private static final long serialVersionUID = 1L;
+        /** The Constant serialVersionUID. */
+        private static final long serialVersionUID = 1L;
 
-    /** deph，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
-    private DepartmentHelper deph = DepartmentHelper.getHelper();
+        /** deph，MemberHelper之物件與Member相關之資料庫方法（Sigleton） */
+        private DepartmentHelper deph = DepartmentHelper.getHelper();
 
-    /**
-     * 處理Http Method請求POST方法（新增資料）
-     *
-     * @param request  Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException      Signals that an I/O exception has occurred.
-     */
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        /**
+         * 處理Http Method請求POST方法（新增資料）
+         *
+         * @param request  Servlet請求之HttpServletRequest之Request物件（前端到後端）
+         * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+         * @throws ServletException the servlet exception
+         * @throws IOException      Signals that an I/O exception has occurred.
+         */
+        /**
+         * public void doPost(HttpServletRequest request, HttpServletResponse response)
+         * throws ServletException, IOException {
+         * 
+         * }
+         */
 
-    }
+        /**
+         * 處理Http Method請求GET方法（取得資料）
+         *
+         * @param request  Servlet請求之HttpServletRequest之Request物件（前端到後端）
+         * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
+         * @throws ServletException the servlet exception
+         * @throws IOException      Signals that an I/O exception has occurred.
+         */
+        public void doGet(HttpServletRequest request, HttpServletResponse response)
+                        throws ServletException, IOException {
+                /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+                JsonReader jsr = new JsonReader(request);
+                /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+                String id = jsr.getParameter("id");
 
-    /**
-     * 處理Http Method請求GET方法（取得資料）
-     *
-     * @param request  Servlet請求之HttpServletRequest之Request物件（前端到後端）
-     * @param response Servlet回傳之HttpServletResponse之Response物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException      Signals that an I/O exception has occurred.
-     */
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+                /** 判斷該字串是否存在，若存在代表要取回個別科別之資料，否則代表要取回全部資料庫內科別之資料 */
+                if (id.isEmpty()) {
+                        /** 透過DepartmentHelper物件之getAll()方法取回所有科別之資料，回傳之資料為JSONObject物件 */
+                        JSONObject query = deph.getAll();
 
-    }
+                        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                        JSONObject resp = new JSONObject();
+                        resp.put("status", "200");
+                        resp.put("message", "所有科別資料取得成功");
+                        resp.put("response", query);
+
+                        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                        jsr.response(resp, response);
+                } else {
+                        /** 透過DepartmentHelper物件的getByID()方法自資料庫取回該科別之資料，回傳之資料為JSONObject物件 */
+                        JSONObject query = deph.getByID(id);
+
+                        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                        JSONObject resp = new JSONObject();
+                        resp.put("status", "200");
+                        resp.put("message", "科別資料取得成功");
+                        resp.put("response", query);
+
+                        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                        jsr.response(resp, response);
+                }
+        }
 
 }
