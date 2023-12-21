e3197f2887b33f6f2f4847c3fa685b768c6f0ac0
M_K_J <kevin911012@gmail.com>
2023-12-22 01:56:55 +0800

    完成UserController內容

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/UserController.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import org.json.*;
+import java.sql.Timestamp;
+import ncu.im3069.demo.app.Clinic;
 import ncu.im3069.demo.app.User;
 import ncu.im3069.demo.app.UserHelper;
 import ncu.im3069.tools.JsonReader;
@@ -38,7 +40,57 @@ public class UserController extends HttpServlet {
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+                JsonReader jsr = new JsonReader(request);
+                JSONObject jso = jsr.getObject();
 
+                /** 取出經解析到JSONObject之Request參數 */
+                String user_id = jso.getString("user_id");
+                String user_name = jso.getString("user_name");
+                String address = jso.getString("address");
+                Timestamp birth = jso.getTimestamp("birth");   
+                String email = jso.getString("email");
+                Boolean gender = jso.getBoolean("gender");
+                String phone = jso.getString("phone");
+                String residence_tel = jso.getString("residence_tel");
+                String blood = jso.getString("blood");
+                float height = jso.getFloat("height");
+                float weight = jso.getFloat("weight");
+                String allergy_history = jso.getString("allergy_history");
+                String servill_history = jso.getString("servill_history");
+                String contact_name = jso.getString("contact_name");
+                String contact_rel = jso.getString("contact_rel");
+                String contact_tel = jso.getString("contact_tel");
+
+                /** 建立一個新的User物件 */
+                User u = new User(user_id, user_name, address, birth, email, gender, phone, residence_tel, blood, height, weight, allergy_history, servill_history, contact_name, contact_rel, contact_tel);
+
+                /** 後端檢查是否有欄位為空值，若有則回傳錯誤訊息 */
+                if (user_id.isEmpty() || user_name.isEmpty() || birth == null || address.isEmpty() || email.isEmpty() || phone.isEmpty() || gender == null) {
+                        /** 以字串組出JSON格式之資料 */
+                        String resp = "{\"status\": \'400\', \"message\": \'欄位不能有空值\', \'response\': \'\'}";
+                        /** 透過JsonReader物件回傳到前端（以字串方式） */
+                        jsr.response(resp, response);
+                }
+                /** 透過UserHelper物件的checkDuplicate()檢查該使用者之身分證字號是否有重複 */
+                else if (!uh.checkDuplicate(u)) {
+                        /** 透過UserHelper物件的create()方法新建一個使用者至資料庫 */
+                        JSONObject data = uh.create(u);
+
+                        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                        JSONObject resp = new JSONObject();
+                        resp.put("status", "200");
+                        resp.put("message", "成功! 註冊使用者資料...");
+                        resp.put("response", data);
+
+                        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                        jsr.response(resp, response);
+                } else {
+                        /** 以字串組出JSON格式之資料 */
+                        String resp = "{\"status\": \'400\', \"message\": \'新增門診失敗，此門診名稱重複！\', \'response\': \'\'}";
+                        /** 透過JsonReader物件回傳到前端（以字串方式） */
+                        jsr.response(resp, response);
+                }
     }
 
     /**
@@ -51,7 +103,37 @@ public class UserController extends HttpServlet {
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+                JsonReader jsr = new JsonReader(request);
+                /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+                String id = jsr.getParameter("id");
 
+                /** 判斷該字串是否存在，若存在代表要取回個別使用者之資料，否則代表要取回全部資料庫內使用者之資料 */
+                if (id.isEmpty()) {
+                        /** 透過UserHelper物件之getAll()方法取回所有使用者之資料，回傳之資料為JSONObject物件 */
+                        JSONObject query = uh.getAll();
+
+                        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                        JSONObject resp = new JSONObject();
+                        resp.put("status", "200");
+                        resp.put("message", "所有使用者資料取得成功");
+                        resp.put("response", query);
+
+                        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                        jsr.response(resp, response);
+                } else {
+                        /** 透過UserHelper物件的getByID()方法自資料庫取回該使用者之資料，回傳之資料為JSONObject物件 */
+                        JSONObject query = uh.getByID(id);
+
+                        /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                        JSONObject resp = new JSONObject();
+                        resp.put("status", "200");
+                        resp.put("message", "使用者資料取得成功");
+                        resp.put("response", query);
+
+                        /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                        jsr.response(resp, response);
+                }
     }
 
     /**
@@ -64,7 +146,23 @@ public class UserController extends HttpServlet {
      */
     public void doDelete(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                JsonReader jsr = new JsonReader(request);
+                JSONObject jso = jsr.getObject();
 
+                /** 取出經解析到JSONObject之Request參數 */
+                String id = jso.getString("id");
+
+                /** 透過UserHelper物件的deleteByID()方法至資料庫刪除該使用者，回傳之資料為JSONObject物件 */
+                JSONObject query = uh.deleteByID(id);
+
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "門診移除成功！");
+                resp.put("response", query);
+
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
     }
 
     /**
@@ -77,6 +175,42 @@ public class UserController extends HttpServlet {
      */
     public void doPut(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+                JsonReader jsr = new JsonReader(request);
+                JSONObject jso = jsr.getObject();
 
+                /** 取出經解析到JSONObject之Request參數 */
+                String user_id = jso.getString("user_id");
+                String user_name = jso.getString("user_name");
+                String address = jso.getString("address");
+                Timestamp birth = jso.getTimestamp("birth");   
+                String email = jso.getString("email");
+                Boolean gender = jso.getBoolean("gender");
+                String phone = jso.getString("phone");
+                String residence_tel = jso.getString("residence_tel");
+                String blood = jso.getString("blood");
+                float height = jso.getFloat("height");
+                float weight = jso.getFloat("weight");
+                String allergy_history = jso.getString("allergy_history");
+                String servill_history = jso.getString("servill_history");
+                String contact_name = jso.getString("contact_name");
+                String contact_rel = jso.getString("contact_rel");
+                String contact_tel = jso.getString("contact_tel");
+
+                /** 透過傳入之參數，新建一個以這些參數之使用者Ussr物件 */
+                User u = new User(user_id, user_name, address, birth, email, gender, phone, residence_tel, blood, null, null, allergy_history, servill_history, contact_name, contact_rel, contact_tel);
+
+                /** 透過User物件的update()方法至資料庫更新該使用者資料，回傳之資料為JSONObject物件 */
+                JSONObject data = u.update();
+
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "成功! 更新使用者資料...");
+                resp.put("response", data);
+
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+        }
     }
 }
