fd38dbc5a3a50166fa41dcb06cd0f6cfe7222938
KennyW901 <113498814+KennyW901@users.noreply.github.com>
2023-12-25 19:59:58 +0800

    Update ScheduleController.java

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/ScheduleController.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/ScheduleController.java
@@ -49,10 +49,41 @@ public class ScheduleController extends HttpServlet {
 //      * @throws ServletException the servlet exception
 //      * @throws IOException      Signals that an I/O exception has occurred.
 //      */
-//     public void doGet(HttpServletRequest request, HttpServletResponse response)
-//             throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+        /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
+        JsonReader jsr = new JsonReader(request);
+        /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
+        String clinic_id = jsr.getParameter("clinic_id");
 
-//     }
+        /** 判斷該字串是否存在，若存在代表要取回個別醫生之資料，否則代表要取回全部資料庫內班表之資料 */
+        if (clinic_id.isEmpty()) {
+                /** 透過ScheduleHelper物件之getAll()方法取回所有班表之資料，回傳之資料為JSONObject物件 */
+                JSONObject query = sh.getAll();
+
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "所有schedule資料取得成功");
+                resp.put("response", query);
+
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+        } else {
+                /** 透過DoctorHelper物件的getByID()方法自資料庫取回該醫生之資料，回傳之資料為JSONObject物件 */
+                JSONObject query = sh.getBySeq(Integer.parseInt(clinic_id));
+
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "班表資料取得成功");
+                resp.put("response", query);
+
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+        }
+
+     }
 
 //     /**
 //      * 處理Http Method請求DELETE方法（刪除）
